733d49cc6e7ec16f1833cd1b157be03a5fe5863f
stephane lorio <lorio.stephane@gmail.com>
2022-08-17 14:36:42 +0200

    finished validations for Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true, allow_blank: false
+  validates :category, inclusion: {
+    in: ['chinese', 'italian', 'japanese', 'french', 'belgian'],
+    message: "%{category} is not a valid"
+  }
 end
---
+++ db/migrate/20220817121125_add_review_reference_to_restaurant.rb
@@ -0,0 +1,5 @@
+class AddReviewReferenceToRestaurant < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :reviews, :restaurant
+  end
+end
